23d915f6cec43240859557c8b6736e15319e1382
memlaw <54655103+memlaw@users.noreply.github.com>
2020-12-07 13:01:11 -0500

    Added click detection to Rogue for side menu

---
+++ Assets/Scripts/Rogue.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rogue : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        SideMenu.Instance.SetRogue();
+    }
+}
---
+++ Assets/Scripts/SideMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SideMenu : MonoBehaviour
+{
+    public static SideMenu Instance;
+
+    public Image Picture;
+
+    public Sprite RogueSprite;
+
+    public SideMenu()
+    {
+        Instance = this;
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void SetRogue()
+    {
+        Picture.sprite = RogueSprite;
+    }
+}
