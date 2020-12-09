4d79207b73d7e78308d9b49296d8b75f857a7c7a
FlashFrost <54652928+FlashFrost@users.noreply.github.com>
2020-12-09 00:08:57 -0600

    Clean Animations, begin Hero Attack

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -24,24 +24,49 @@ public class Hero : MonoBehaviour
     public int Experience;
     public int MaxHealth;
     public int Health;
-    public int AttackSplashRange;
+    public float AttackSplashRange;
     public int HealAbility = 0;
 
     public StartableLocation Position;
 
+
     private int reviveCost = 50;
     private Animator animator;
     private bool Gender;
+    private bool attacking;
+    private const int monsterLayer = 8;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
+        MaxHealth = Health;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!attacking)
+        {
+            CheckNearObjects();
+        }
+
+        if (attacking)
+        {
+
+        }
+    }
+
+    void CheckNearObjects()
+    {
+        Collider[] objects = Physics.OverlapSphere(transform.position, Range);
+
+        foreach (Collider unit in objects)
+        {
+            if (unit.tag == "Enemy")
+            {
+                //There are enemies in range, Choose a target to attack.
+            }
+        }
     }
 
     void OnMouseDown()
@@ -61,9 +86,9 @@ public class Hero : MonoBehaviour
     {
         if(MyType == HeroType.Cleric)
         {
-            MaxHealth += 15;
-            Health += 15;
-            Damage += 2;
+            MaxHealth += 10;
+            Health += 10;
+            Damage += 1;
             Level += 1;
             if(Level >= 1)
             {
@@ -72,10 +97,9 @@ public class Hero : MonoBehaviour
         }
         else if(MyType == HeroType.Ranger)
         {
-            MaxHealth += 15;
-            Health += 15;
+            MaxHealth += 5;
+            Health += 5;
             Damage += 2;
-            AttackSpeed += 1;
             Level += 1;
             if (Level >= 1)
             {
@@ -87,38 +111,35 @@ public class Hero : MonoBehaviour
             MaxHealth += 15;
             Health += 15;
             Damage += 2;
-            Range += 1;
-            AttackSpeed += 1;
             Level += 1;
             if (Level >= 1)
             {
-                HealAbility += 5;
+                AttackSplashRange += 0.25f;
+                MaxHealth += 10;
+                Health += 10;
+                Damage += 2;
             }
         }
         else if(MyType == HeroType.Rogue)
         {
-            MaxHealth += 15;
-            Health += 15;
-            Damage += 2;
-            Range += 1;
-            AttackSpeed += 1;
+            MaxHealth += 3;
+            Health += 3;
+            Damage += 1;
             Level += 1;
             if (Level >= 1)
             {
-                HealAbility += 5;
+                AttackSpeed += 3;
             }
         }
         else if(MyType == HeroType.Wizard)
         {
-            MaxHealth += 15;
-            Health += 15;
-            Damage += 2;
-            Range += 1;
-            AttackSpeed += 1;
+            MaxHealth += 1;
+            Health += 1;
             Level += 1;
             if (Level >= 1)
             {
-                HealAbility += 5;
+                Damage += 10;
+                AttackSplashRange += 1;
             }
         }
     }
@@ -132,4 +153,5 @@ public class Hero : MonoBehaviour
             animator.SetTrigger("Death");
         }
     }
+
 }
